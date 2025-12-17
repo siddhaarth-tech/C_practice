739cd7e66e60b169a2824482208376af2bdfe3b9
siddhaarth-tech <siddhaarth@mobicip.com>
2025-12-18 01:40:40 +0530

    Create ex1_11.c

---
+++ Chapter1/ex1_11.c
@@ -0,0 +1,8 @@
+/*How would you test the word count program? What kinds of input are most
+likely to uncover bugs if there are any?*/
+
+Empty input //EOF
+Input with only whitespace // 
+Single word without trailing newline //hello
+Multiple spaces //                           
+Tabs mixed with spaces //      
