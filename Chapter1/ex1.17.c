91bf906e1e7cedf7da357349ef9053308debb36c
siddhaarth-tech <siddhaarth@mobicip.com>
2025-12-18 12:46:09 +0530

    Update ex1.17.c

--- Chapter1/ex1.17.c
+++ Chapter1/ex1.17.c
@@ -1,3 +1,5 @@
+/*Write a program to print all input lines that are longer than 80 characters.
+Exercise 1-18. Write a program to remove trailing blanks and tabs from each line of inpu*/
 #include <stdio.h>
 #define MAXLINE 1000
 
