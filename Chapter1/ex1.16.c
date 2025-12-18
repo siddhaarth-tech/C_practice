4acb9444520ec7b938a96c9a9f350756f060ed1b
siddhaarth-tech <siddhaarth@mobicip.com>
2025-12-18 12:45:23 +0530

    Update ex1.16.c

--- Chapter1/ex1.16.c
+++ Chapter1/ex1.16.c
@@ -1,3 +1,5 @@
+/*Revise the main routine of the longest-line program so it will correctly print
+the length of arbitrary long input lines, and as much as possible of the text.*/
 #include <stdio.h>
 #define MAXLINE 1000
 
