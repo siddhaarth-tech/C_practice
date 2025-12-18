7b2f1d95881f83e27f34e7c4ae6749ad60983017
siddhaarth-tech <siddhaarth@mobicip.com>
2025-12-18 12:58:09 +0530

    Update ex1.19.c

--- Chapter1/ex1.19.c
+++ Chapter1/ex1.19.c
@@ -1,28 +1,60 @@
+/*Write a function reverse(s) that reverses the character string s. Use it to
+write a program that reverses its input a line at a time.*/
 #include <stdio.h>
-#include <string.h>
 #define MAXLINE 1000
-
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
 void reverse(char s[]);
 
 int main() {
     char line[MAXLINE];
+    char copied[MAXLINE];
+    int len;
 
-    while (fgets(line, MAXLINE, stdin) != NULL) {
-        reverse(line);
-        printf("%s", line);
+    while ((len = getline(line, MAXLINE)) > 0) {
+        copy(copied, line);
+        reverse(copied);
+        printf("%s", copied);
     }
 
     return 0;
 }
 
+int getline(char s[], int lim) {
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+        s[i] = c;
+
+    if (c == '\n') {
+        s[i] = c;
+        i++;
+    }
+
+    s[i] = '\0';
+    return i;
+}
+void copy(char to[], char from[]) {
+    int i = 0;
+
+    while ((to[i] = from[i]) != '\0')
+        i++;
+}
 void reverse(char s[]) {
-    int i, j;
+    int i = 0, j;
     char temp;
-    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
-        if (s[j] == '\n') { j--; } 
+
+    for (j = 0; s[j] != '\0'; j++)
+        ;
+    j--; 
+    if (s[j] == '\n')
+        j--;
+
+    while (i < j) {
         temp = s[i];
         s[i] = s[j];
         s[j] = temp;
+        i++;
+        j--;
     }
 }
-
