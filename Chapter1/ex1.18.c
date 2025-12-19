d5af6790c9b483f5104f0b2de6553c4457dc7c93
siddhaarth-tech <siddhaarth@mobicip.com>
2025-12-19 16:40:22 +0530

    Update ex1.18.c

--- Chapter1/ex1.18.c
+++ Chapter1/ex1.18.c
@@ -3,13 +3,13 @@ and to delete entirely blank lines.*/
 #include <stdio.h>
 #define MAXLINE 1000
 
-int getline(char line[], int maxline);
+int getline_custom(char line[], int maxline);
 
 int main() {
     int len, i;
     char line[MAXLINE];
 
-    while ((len = getline(line, MAXLINE)) > 0) {
+    while ((len = getline_custom(line, MAXLINE)) > 0) {
         for (i = len - 1; i >= 0 && (line[i] == ' ' || line[i] == '\t' || line[i] == '\n'); --i)
             ;
         line[i + 1] = '\n';
@@ -21,4 +21,22 @@ int main() {
 
     return 0;
 }
+int getline_custom(char s[], int lim) {
+    int c, i = 0, len = 0;
 
+    while ((c = getchar()) != EOF && c != '\n') {
+        len++;
+        if (i < lim - 1) {
+            s[i++] = c; 
+        }
+    }
+
+    if (c == '\n') {
+        len++;
+        if (i < lim - 1)
+            s[i++] = c;
+    }
+
+    s[i] = '\0';
+    return len; 
+}
