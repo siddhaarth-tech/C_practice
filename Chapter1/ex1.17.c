acb0742fb896cd3c33de0829a0e72fcb03eb572d
siddhaarth-tech <siddhaarth@mobicip.com>
2025-12-19 16:38:42 +0530

    Update ex1.17.c

--- Chapter1/ex1.17.c
+++ Chapter1/ex1.17.c
@@ -1,15 +1,15 @@
-/*Write a program to print all input lines that are longer than 80 characters.
-Exercise 1-18. Write a program to remove trailing blanks and tabs from each line of inpu*/
+/*Write a program to print all input lines that are longer than 80 characters.*/
+
 #include <stdio.h>
 #define MAXLINE 1000
 
-int getline(char line[], int maxline);
+int getline_custom(char line[], int maxline);
 
 int main() {
     int len;
     char line[MAXLINE];
 
-    while ((len = getline(line, MAXLINE)) > 0) {
+    while ((len = getline_custom(line, MAXLINE)) > 0) {
         if (len > 80)
             printf("%s", line);
     }
@@ -17,3 +17,22 @@ int main() {
     return 0;
 }
 
+int getline_custom(char s[], int lim) {
+    int c, i = 0, len = 0;
+
+    while ((c = getchar()) != EOF && c != '\n') {
+        len++;
+        if (i < lim - 1) {
+            s[i++] = c; 
+        }
+    }
+
+    if (c == '\n') {
+        len++;
+        if (i < lim - 1)
+            s[i++] = c;
+    }
+
+    s[i] = '\0';
+    return len; 
+}
